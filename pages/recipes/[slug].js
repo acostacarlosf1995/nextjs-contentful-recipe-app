a4997d85ea926eed1ff5eb0513a2a5b416774be9
Carlos Acosta <carlosacostaf1995@gmail.com>
2022-08-10 13:47:29 -0500

    second commit

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -2,6 +2,8 @@ import { createClient } from 'contentful'
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
+import Skeleton from "../../components/Skeleton";
+
 const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
@@ -10,6 +12,8 @@ const client = createClient({
 export default function RecipeDetails({ recipe }) {
 
     console.log(recipe)
+
+    if (!recipe) return <Skeleton />
 
     const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
 
@@ -90,7 +94,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false
+        fallback: true
     }
 }
 
